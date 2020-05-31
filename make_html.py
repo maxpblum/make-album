eb2fe9952cefda75e39a56759cdfff31400ee1a0
Max Blum-Campo <max.blum@gmail.com>
2020-05-31 11:22:14 -0400

    Much more flexible layout

--- make_html.py
+++ make_html.py
@@ -3,27 +3,38 @@ import json
 import argparse
 import re
 
+
 class Sizing(object):
-    def __init__(self, width_units, height_units, margin_units,
-                 space_between_photos_units, pixels_per_unit, photos_per_row,
-                 rows_per_page):
-        self.page_width = width_units * pixels_per_unit
-        self.page_height = height_units * pixels_per_unit
-        self.margin = margin_units * pixels_per_unit
-        self.gap = space_between_photos_units * pixels_per_unit
-        self.photos_per_row = photos_per_row
-        self.rows_per_page = rows_per_page
+    def __init__(
+            self,
+            width_units,
+            height_units,
+            margin_units,
+            space_between_photos_units,
+            pixels_per_unit,
+            rows_per_page,
+            columns_per_page,
+            photos_per_page,
+    ):
+        self.photos_per_page = photos_per_page
 
-        default_photo_width = self.page_width - (2 * self.margin) - (
-            (photos_per_row - 1) * self.gap)
-        default_photo_height = self.page_width - (2 * self.margin) - (
-            (rows_per_page - 1) * self.gap)
-        self.aspect = default_photo_width * 1.0 / default_photo_height
+        units_to_pixels = lambda units: units * pixels_per_unit
 
-    def __str__(self):
-        return 'page width: {}\npage height: {}\nmargin: {}\ngap: {}\nphotos per row: {}\nrows per page: {}\ndefault image aspect ratio: {}'.format(
-            self.page_width, self.page_height, self.margin, self.gap,
-            self.photos_per_row, self.rows_per_page, self.aspect)
+        page_margin = units_to_pixels(margin_units)
+        # gap = Space between photos.
+        gap = units_to_pixels(space_between_photos_units)
+        self.photo_padding = (gap / 2.0)
+        # Achieve desired page padding by accounting for photo padding.
+        self.page_padding = page_margin - (gap / 2.0)
+
+        self.page_width = units_to_pixels(width_units)
+        self.page_height = units_to_pixels(height_units)
+
+        self.row_height = 1.0 * (self.page_height -
+                                 (2 * self.page_padding)) / rows_per_page
+        self.column_width = 1.0 * (self.page_width -
+                                   (2 * self.page_padding)) / columns_per_page
+
 
 # Gets the substring after the first instance of first and before the next
 # instance of second.
@@ -45,70 +56,56 @@ class Template(object):
         self.image_template = get_substring_between(template,
                                                     '<!-- for_each_image -->',
                                                     '<!-- end_for -->')
-        self.line_template = get_substring_between(template,
-                                                   '<!-- for_each_line -->',
-                                                   '<!-- end_for -->')
         self.page_template = get_substring_between(template,
                                                    '<!-- for_each_page -->',
                                                    '<!-- end_for -->')
 
     def fill(self, sizing, pages):
-        no_aspect = 0
         filled_pages = []
         for page in pages:
-            filled_lines = []
-            for line in page['lines']:
-                filled_images = []
-                for image in line['images']:
-                    if 'aspect' not in image:
-                        no_aspect += 1
-                        image['aspect'] = 1.0
-                    image_aspect = image['aspect']
-                    if image_aspect >= sizing.aspect:
-                        image_style = 'height: 100%'
-                    else:
-                        image_style = 'width: 100%'
-                    new_filled_image = self.image_template.replace(
-                        'image_code', image['code']).replace(
-                            'image_filename',
-                            image['filename']).replace('image_style',
-                                                       image_style)
-                    filled_images.append(new_filled_image)
-                filled_images_string = ''.join(filled_images)
-                new_filled_line = self.line_template.replace(
-                    'line_content', filled_images_string)
-                filled_lines.append(new_filled_line)
-            filled_lines_string = ''.join(filled_lines)
-            new_filled_page = self.page_template.replace(
-                'page_content', filled_lines_string).replace(
-                    'page_code', page['code']).replace('page_class',
-                                                       'page-with-rows')
+            filled_images = []
+            for image in page['images']:
+                new_filled_image = (
+                    self.image_template  #
+                    .replace('image_code', image['code'])  #
+                    .replace('image_filename', image['filename']))
+
+                filled_images.append(new_filled_image)
+            filled_images_string = ''.join(filled_images)
+            new_filled_page = (
+                self.page_template  #
+                .replace('page_content', filled_images_string)  #
+                .replace('page_code', page['code'])  #
+                .replace('page_class', 'page-with-rows'))
             filled_pages.append(new_filled_page)
-        return '{}{}{}'.format(self.pre_content, ''.join(filled_pages),
-                               self.post_content)
+        return ('{}{}{}'.format(self.pre_content, ''.join(filled_pages),
+                                self.post_content)  #
+                .replace('page_height', '{}'.format(sizing.page_height))  #
+                .replace('page_width', '{}'.format(sizing.page_width))  #
+                .replace('page_padding', '{}'.format(sizing.page_padding))  #
+                .replace('row_height', '{}'.format(sizing.row_height))  #
+                .replace('column_width', '{}'.format(sizing.column_width))  #
+                .replace('photo_padding', '{}'.format(sizing.photo_padding))  #
+                )
+
+
 def build_pages(sizing, sorted_images):
     pages = []
     current_page = None
-    current_line = None
     for image in sorted_images:
-        if current_line is not None and len(current_line['images']) >= sizing.photos_per_row:
-            current_page['lines'].append(current_line)
-            current_line = None
-        if current_line is None:
-            current_line = {'images': []}
-
-        if current_page is not None and len(current_page['lines']) >= sizing.rows_per_page:
+        if current_page is not None and len(
+                current_page['images']) >= sizing.photos_per_page:
             pages.append(current_page)
             current_page = None
         if current_page is None:
-            current_page = {'code': 'p{}'.format(len(pages) + 1), 'lines': []}
+            current_page = {'code': 'p{}'.format(len(pages) + 1), 'images': []}
 
-        current_line['images'].append({'code': image['code'], 'filename': image['image_file'], 'aspect': image['aspect']})
+        current_page['images'].append({
+            'code': image['code'],
+            'filename': image['image_file']
+        })
 
-    if current_line is not None and len(current_line['images']) > 0:
-        current_page['lines'].append(current_line)
-        
-    if current_page is not None and len(current_page['lines']) > 0:
+    if current_page is not None and len(current_page['images']) > 0:
         pages.append(current_page)
 
     return pages
@@ -126,9 +123,16 @@ parser.add_argument(
     help='directory in which to find files (omit closing slash)')
 args = parser.parse_args()
 
-sizing = Sizing(width_units=12, height_units=12, margin_units=0.5,
-        space_between_photos_units=0.25, pixels_per_unit=300, photos_per_row=3,
-        rows_per_page=3)
+sizing = Sizing(
+    width_units=12,
+    height_units=12,
+    margin_units=0.5,
+    space_between_photos_units=0.25,
+    pixels_per_unit=300,
+    rows_per_page=3,
+    columns_per_page=3,
+    photos_per_page=6,
+)
 
 with open('photo_album_template.html', 'r') as f:
     template = f.read()
