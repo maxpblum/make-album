c4c7c05980f8f7029ab93b1402b583dff9d3f3d2
Max Blum-Campo <max.blum@gmail.com>
2020-06-11 19:32:12 -0400

    Split render function out

--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import { hotReload, loadOnce } from './hotReload.mjs';
+import renderLayout from './render.mjs';
 import * as domUtil from './domUtil.mjs';
 import * as funcUtil from './funcUtil.mjs';
 
@@ -92,69 +93,6 @@ function getChangeInfo(oldPagination, newPagination) {
     lastUnchangedPage: null,
     changedPagination: newPagination,
   };
-}
-
-function renderLayout(lastUnchangedPage, changedPagination) {
-  const pages = document.body.children;
-  for (let i = pages.length - 1; i >= 0; i--) {
-    if (pages[i] === lastUnchangedPage) break;
-    document.body.removeChild(pages[i]);
-  }
-
-  let newPage;
-
-  funcUtil.promisersListToPromiseChain(changedPagination.map(item => () => {
-
-    if (!newPage) newPage = domUtil.getNewPage();
-
-    if (item.startsWith('break')) {
-      // render a manual tag to find the right spot.
-      const debugTag = document.createElement('p');
-      debugTag.className = 'debug-tag';
-      debugTag.innerHTML = item;
-      document.body.appendChild(debugTag);
-
-      newPage = domUtil.getNewPage();
-      return;
-    }
-
-    if (item === 'rows' || item === 'columns') {
-      domUtil.forceDirection(newPage.children[0], item);
-      return;
-    }
-
-    const photoCode = item.split(' ')[0];
-    return domUtil.addPhotoToParent(
-      window.photoMap[photoCode].image_file,
-      item,
-      newPage.children[0],
-    ).then(photoEl => {
-      if (domUtil.checkOverflow(newPage.children[0])) {
-        const toggleIfUnforced = () => {
-          if (newPage.children[0].className.indexOf('direction-forced') === -1) {
-            domUtil.toggleDirection(newPage.children[0]);
-          }
-        }
-        toggleIfUnforced();
-        if (domUtil.checkOverflow(newPage.children[0])) {
-          toggleIfUnforced();
-          domUtil.removePhotoFromParent(photoEl, item, newPage.children[0]);
-          newPage = domUtil.getNewPage();
-          return domUtil.addPhotoToParent(
-            window.photoMap[photoCode].image_file,
-            item,
-            newPage.children[0],
-          );
-        }
-        // if we did not execute the above, switching the flow direction fixed
-        // the overflow issue
-      }
-    });
-  })).then(() => {
-    if (newPage && newPage.children && newPage.children.length === 0) {
-      document.body.removeChild(newPage);
-    }
-  });
 }
 
 window.addEventListener(
